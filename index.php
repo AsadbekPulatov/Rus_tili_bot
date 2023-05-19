2f83e9b3770c6afc01f28d505d82f02646e54094
Lenovo <asadbek.polatov.02@mail.ru>
2023-05-19 21:32:10 +0500

    bug fixed

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
-    <title>Dars qo'shish</title>
+    <title>Darslar ro'yxati</title>
 </head>
 <body>
 <h1 align="center" class="text text-primary container">Darslar ro'yxati</h1>
